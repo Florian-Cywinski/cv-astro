99f51a407e46716533ff4633eebd786e638b5e35
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-06-13 16:18:19 +0200

    Header - left panel - profile picture

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind({
+    // Example: Provide a custom path to a Tailwind config file
+    configFile: './tailwind.config.mjs',
+    applyBaseStyles: false,
+    nesting: true
+  }), icon()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateColumns:
+			{
+			  '25/75': '25% 75%',
+			}
+		  },
 	},
 	plugins: [],
 }
