28383420920157db6ee4cd2272fbc96f9851a885
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-06-14 16:21:03 +0200

    added icons and custom fonts + colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,15 @@ export default {
 			gridTemplateColumns:
 			{
 			  '25/75': '25% 75%',
-			}
+			},
+			colors: {   // To add custom color classes
+				primary: '#34404a',		// text-primary
+				secondary: '#f1f1f1',	// text-secondary
+			  },
+			  fontFamily: { // To add custom fonts 
+				roboto: ['Roboto', 'sans-serif'], 	// A Google font
+				museoModerno: ['MuseoModerno', 'sans-serif'],	// A Google font - <p class="font-museoModerno darkGray">MMMWWWMMM</p>
+			  },
 		  },
 	},
 	plugins: [],
